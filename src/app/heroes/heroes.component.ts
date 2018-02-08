0c33ed99e9fa135917ffeb761ff260eb3f4e184a
unknown <flavius.cater@gmail.com>
2018-02-08 22:41:21 +0200

    Listed heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero'
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
+  heroes = HEROES;
+
+  hero : Hero = {
+    id: 1,
+    name: 'Windstorm'
+  }
+
   constructor() { }
 
   ngOnInit() {
   }
 
-  hero = 'Windstorm';
+ 
 }
